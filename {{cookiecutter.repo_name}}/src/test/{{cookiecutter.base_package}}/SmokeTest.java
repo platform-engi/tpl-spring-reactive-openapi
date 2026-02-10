ae354a476f947515faa770487ad7398612e81ed9
Gabriel Benavides <grilles_telling.7v@icloud.com>
2026-02-10 02:05:36 -0500

    version 2.0.0 template

---
+++ {{cookiecutter.repo_name}}/src/test/{{cookiecutter.base_package}}/SmokeTest.java
@@ -0,0 +1,10 @@
+package {{cookiecutter.base_package}};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+  @Test
+  void contextLoads() {}
+}
